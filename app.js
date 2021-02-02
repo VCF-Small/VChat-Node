4caf5b86af12d6effe683e911d6951f32cd449fe
ankit <ac2000choudhary@gmail.com>
2021-02-02 13:23:17 +0530

    main app file

--- app.js
+++ app.js
@@ -1,9 +1,59 @@
-var express = require("express");
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var expressValidator = require('express-validator');
+var flash = require('express-flash');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+ 
+ 
+var mysql = require('mysql');
+var connection  = require('./db');
 
+//var indexRouter = require('./index');
+var authRouter = require('./auth');
 
 var app = express();
+ 
+// view engine setup
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'Views')));
 
-app.set("port", 8000);
-app.listen(app.get("port"), function(){
-    console.log("listning");
-});
+app.use(session({ 
+    secret: '123456cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 }
+}));
+
+
+app.use(flash());
+app.use(expressValidator());
+// app.use('view-engine', 'ejs');
+app.set('view engine', 'ejs');
+ 
+app.use('/auth', authRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+  });
+   
+  // error handler
+  app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+   
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
+  
+  app.listen(3000);
+  module.exports = app;
